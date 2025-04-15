07f40a53beb06188c983570357e47e50cc460293
Kevin Mensah <kevin.mensah@fireflyelectric.com>
2025-04-15 13:21:31 +0800

    first commit

---
+++ docker/new-laravel-project/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Add your other protected routes here
+});
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'Hello, world!']);
+});
